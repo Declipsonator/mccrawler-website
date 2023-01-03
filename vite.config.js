07dfbb89d2c09573a6c18b55b673429ee7735547
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2023-01-03 16:35:07 +0000

    Work with static website

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -6,5 +7,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'search/index.html'),
+      },
+    },
   }
 })
