e7bad0cc6d3f780e1145b112e6f8caccd67dfe77
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2023-01-03 17:25:14 +0000

    add 404 idk if this will work

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
     host: '0.0.0.0',
   },
